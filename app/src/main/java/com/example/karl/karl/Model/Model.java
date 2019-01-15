a45477b01547dc0d6e71acf2360632a6e68a3816
Lisa Griere <lisa.gr@hotmail.fr>
2019-01-15 15:59:57 +0100

    Ajout fonctionnalite swipe

---
+++ app/src/main/java/com/example/karl/karl/Model/Model.java
@@ -0,0 +1,30 @@
+package com.example.karl.karl.Model;
+
+/**
+ * Created by Lisa on 14/01/2019.
+ */
+
+public class Model {
+    public String title, image;
+
+    public Model(String title, String image){
+        this.title = title;
+        this.image = image;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setImage(String image){
+        this.image = image;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getImage(){
+        return image;
+    }
+}
